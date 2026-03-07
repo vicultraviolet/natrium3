aa7087e43ef1fb248be5f026efa7d87d63ddcd75
goofyahhvic <95176727+goofyahhvic@users.noreply.github.com>
2026-03-07 04:01:58 -0300

    updated asset paths in `sandbox`

--- examples/sandbox/sandbox.rs
+++ examples/sandbox/sandbox.rs
@@ -41,12 +41,12 @@ fn main()
     let mut app = App::new();
 
     app.set_window_context(WindowContext::new(1000, "Sandbox"));
-    app.set_asset_context(AssetContext::new(PathBuf::from(r"asset_registry.json")));
+    app.set_asset_context(AssetContext::new(PathBuf::from(r"assets/asset_registry.json")));
 
     let hello_world = app.get_asset_context().new_asset("helloWorld", TextAsset::new);
 
     hello_world.set_data(String::from("Hello, world!"));
-    let _ = hello_world.save(Path::new("hello_world.txt"));
+    let _ = hello_world.save(Path::new("assets/hello_world.txt"));
     if let Err(e) = app.get_asset_context().save_registry()
     {
         println!("Failed to save asset registry: {}", e)
